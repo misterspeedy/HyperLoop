1741e856bb77aae8926081a17cae15266bac188d
Kit Eason <kit.eason@gmail.com>
2014-09-30 08:41:10 +0100

    Accumulate recorded bytes in a buffer

--- Harness/Harness.fs
+++ Harness/Harness.fs
@@ -1,8 +1,25 @@
 ﻿open System
 open HyperLoop
 
+type private DummyListener() =
+    let _onCommand = new Event<ListenerCommandDelegate, ListenerCommandArgs>()
+    member this.Do cmd = 
+        _onCommand.Trigger(this, new ListenerCommandArgs(cmd))
+    interface IListener with
+        [<CLIEvent>]
+        member this.OnCommand = _onCommand.Publish
+
 [<EntryPoint>]
 let main argv = 
-    // use controller = new Controller(4)
-
-    0 // return an integer exit code
+   let listener = new DummyListener()
+   let waveIn = new NAudio.Wave.WaveInEvent()
+   waveIn.BufferMilliseconds <- 100
+   waveIn.NumberOfBuffers <- 2
+   waveIn.DeviceNumber <- 0   
+   let controller = new Controller(1, listener, waveIn)
+   listener.Do("D0")
+   System.Threading.Thread.Sleep 1500
+   listener.Do("D0")
+   printfn "Press a key"
+   Console.ReadKey(false) |> ignore
+   0 // return an integer exit code
--- HyperLoop/Recorder.fs
+++ HyperLoop/Recorder.fs
@@ -1,24 +1,34 @@
 ﻿namespace HyperLoop
 
+open System
 open NAudio.Wave
 
 // TODO Needs its own tests
 
 /// Takes an NAudio IWaveIn and records, stops recording,
 /// pauses, plays back etc. under the control of a Toggler
-type Recorder(waveInEvent : IWaveIn) as this = 
+type Recorder(waveIn : IWaveIn) as this = 
    // TODO Inject the toggler?
    let _toggler = Toggler()
+
+   let _bytes = ResizeArray<byte>()
    
-   let dataAvailable args =
-     printfn "Data available"
+   let dataAvailable (args : WaveInEventArgs) =
+      // printfn "Data available"
+      // TODO make faster!
+      for i in 0..args.BytesRecorded-1 do
+         _bytes.Add(args.Buffer.[i])
+           
    let recordingStopped args =
-     printfn "Recording stopped"
+      printfn "Recording stopped"
+      printfn "Buffer length: %i" _bytes.Count
+//      for b in _bytes do
+//         printf "%A|" b
 
    do
       // TODO magic numbers
       // TODO constructor params
-      waveInEvent.WaveFormat = new WaveFormat(44100, 1) |> ignore
+      waveIn.WaveFormat = new WaveFormat(44100, 1) |> ignore
    do
       _toggler.OnToggle.Add (fun (_, oldState, state) -> 
          match _toggler.State with
@@ -26,27 +36,30 @@ type Recorder(waveInEvent : IWaveIn) as this =
          | ToggleState.Recording -> this.Record oldState
          | ToggleState.Playing -> this.Play oldState
          | ToggleState.Muted -> this.Mute oldState
-         | _ -> failwith "Unsupported toggle state"
       )
-      waveInEvent.DataAvailable.Add(fun args -> dataAvailable args)
-      waveInEvent.RecordingStopped.Add(fun args -> recordingStopped args)
+      waveIn.DataAvailable.Add(fun args -> dataAvailable args)
+      waveIn.RecordingStopped.Add(fun args -> recordingStopped args)
 
    let stopIfRecording oldState =
       if oldState = ToggleState.Recording then
-         waveInEvent.StopRecording()
+         waveIn.StopRecording()
 
    member __.Toggler = _toggler
    member __.Reset oldState = 
-      printfn "Reset"
       stopIfRecording oldState
+      _bytes.Clear()
    member __.Record oldState = 
-      printfn "Recording"
       stopIfRecording oldState
-
-      waveInEvent.StartRecording()
+      waveIn.StartRecording()
    member __.Play oldState =
-      printfn "Playing"
+      // Playing doesn't mean actively doing anything - just make the record buffer
+      // available
       stopIfRecording oldState
    member __.Mute oldState = 
-      printfn "Muted"
       stopIfRecording oldState
+   // Next - think about what is going to do the playing
+   // Add unit tests for Buffer member if kept
+   member __.Buffer =
+      if _toggler.State <> Playing then
+         raise (Exception("Cannot access buffer if not in Playing state"))
+      _bytes
