d44a57a1e9ae4bd9b30898685eb8d350888c8066
Kit Eason <kit.eason@gmail.com>
2014-11-06 08:15:28 +0000

    Get crossfader working

--- HyperLoop/CrossFader.fs
+++ HyperLoop/CrossFader.fs
@@ -1,4 +1,14 @@
 ﻿module CrossFader
 
-let Fade (count : int) (buffer1 : byte[]) (buffer2 : byte[]) =
-   buffer1, buffer2
+let inline private (~~) x = double x
+
+let CrossFade (count : int) (buffer1 : byte[]) (buffer2 : byte[]) =
+   // TODO check equal lengths
+   let region1 = buffer1.[buffer2.Length-count..]
+   let region2 = buffer2.[..count-1]
+   let both = Array.zip region1 region2
+   both 
+   |> Array.mapi (fun i (a, b) ->
+         let m1 = ~~i / ~~count
+         let m2 = 1. - m1
+         ~~a * m2 + ~~b * m1)
--- HyperLoopTests/CrossFaderTests.fs
+++ HyperLoopTests/CrossFaderTests.fs
@@ -7,26 +7,42 @@ open FsUnit
 open HyperLoop
 
 [<Test>]
-let ``The cross fader returns two buffers with the same length as the originals``() = 
-   let expected = 10, 10
+let ``The cross fader returns a buffer of length count``() = 
+   let expected = 3
    let buffer1, buffer2 = Array.zeroCreate 10, Array.zeroCreate 10
-   let result1, result2 = CrossFader.Fade 3 buffer1 buffer2
-   let actual = result1.Length, result2.Length
-   Assert.AreEqual(actual, expected)
+   let result = CrossFader.CrossFade 3 buffer1 buffer2
+   let actual = result.Length
+   actual |> should equal expected
 
 [<Test>]
-let ``When the originals have the same levels the results are the same as the originals``() = 
-   let buffer1 = Array.init 10 (fun i -> i |> byte)
+let ``When the originals have the same levels the result has the same level as the originals for the splice region``() = 
+   let buffer1 = Array.init 10 (fun _ -> 128uy)
    let buffer2 = buffer1
-   let expected = buffer1, buffer2
-   let actual = CrossFader.Fade 3 buffer1 buffer2
-   Assert.AreEqual(actual, expected)
+   let expected = buffer1.[7..]
+   let actual = CrossFader.CrossFade 3 buffer1 buffer2
+   actual |> should equal expected
 
 [<Test>]
-let ``Results outside the cross fade count are unchanged``() = 
-   let buffer1 = Array.init 10 (fun i -> i |> byte)
-   let buffer2 = Array.init 10 (fun i -> 9-i |> byte)
-   let expected = buffer1.[..6], buffer2.[3..]
-   let result1, result2 = CrossFader.Fade 3 buffer1 buffer2
-   let actual = result1.[..6], result2.[3..]
-   Assert.AreEqual(actual, expected)
+let ``When one original has 0 levels and the other has non-zero levels there is a linear fade from 0 to the non-zero value``() = 
+   let buffer1 = Array.init 10 (fun _ -> 0uy)
+   let buffer2 = Array.init 10 (fun _ -> 100uy)
+   let expected = [|0; 10; 20; 30; 40; 50; 60; 70; 80; 90|] |> Array.map byte
+   let actual = CrossFader.CrossFade 10 buffer1 buffer2
+   actual |> should (equalWithin 10E-5) expected
+
+[<Test>]
+let ``When one original has non-zero levels and the other has 0 levels there is a linear fade from the non-zero value to 0``() = 
+   let buffer1 = Array.init 10 (fun _ -> 100uy)
+   let buffer2 = Array.init 10 (fun _ -> 0uy)
+   let expected = [|100; 90; 80; 70; 60; 50; 40; 30; 20; 10|] |> Array.map byte
+   let actual = CrossFader.CrossFade 10 buffer1 buffer2
+   actual |> should (equalWithin 10E-5) expected
+
+[<Test>]
+let ``When two arbitrary signals are cross faded they are blended correctly``() = 
+   let buffer1 = [|30; 10; 40; 10; 20|] |> Array.map byte
+   let buffer2 = [|50; 40; 20; 30; 90|] |> Array.map byte
+   let expected = [|30; 16; 32; 22; 76|] |> Array.map byte
+   let actual = CrossFader.CrossFade 5 buffer1 buffer2
+   actual |> should (equalWithin 10E-5) expected
+
